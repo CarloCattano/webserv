f1c33800c85683706d83e075d1d0d324dacc080a
JalbersCode <josephal@hotmail.de>
2024-03-17 19:55:07 +0100

    here we go

--- src/Config/Config.cpp
+++ src/Config/Config.cpp
@@ -1,5 +1,4 @@
 #include "Config.hpp"
-
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -14,36 +13,102 @@ Config::Config()
 
 Config::~Config()
 {
+
 }
 
-void Config::getPort(std::string line)
-{
-	std::string delimiter = " ";
-	size_t pos = 0;
-	std::string token;
-	if (line.empty()) {
-		std::cerr << "Error: empty line" << std::endl;
+// void getPort(std::string line)
+// {
+// 	std::string delimiter = " ";
+// 	size_t pos = 0;
+// 	std::string token;
+// 	if (line.empty()) {
+// 		std::cerr << "Error: empty line" << std::endl;
+// 	}
+// 	else {
+// 		if ((pos = line.find("listen")) != std::string::npos) {
+// 			line.erase(0, line.length() - 5);
+// 			line.erase(line.length() - 1); // TODO : adjust for any port length
+// 			std::stringstream ss(line);
+// 			ss >> this->port;
+// 		}
+// 	}
+// }
+
+std::string get_file_content(const std::string &filename) {
+	std::ifstream file(filename.c_str());
+	std::string line;
+	std::string file_content;
+
+    if (file.is_open()) { 
+        while (std::getline(file, line)) {
+            file_content += line + '\n';
+        }
+        file.close();
+    } else {
+        std::cerr << "Failed to open the file." << std::endl;
+		// throw error????
+    }
+	file_content += '\0'; 
+	return (file_content);
+}
+
+bool is_whitespace_char(char c) {
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (true);
+	else
+		return (false);	
+}
+
+Virtual_Server_Config read_virtual_server_obj(std::string str, int i) {
+	Virtual_Server_Config virtual_server;
+
+	while (is_whitespace_char(str[i])) {i++;}
+	if (str[i++] != '{') {
+		throw std::runtime_error("Starting curly brace missing: '{'");
 	}
-	else {
-		if ((pos = line.find("listen")) != std::string::npos) {
-			line.erase(0, line.length() - 5);
-			line.erase(line.length() - 1); // TODO : adjust for any port length
-			std::stringstream ss(line);
-			ss >> this->port;
+	while (str[i] && str[i] != '}') {
+		std::vector<std::string> values;
+		while (is_whitespace_char(str[i])) {i++;}
+		while (str[i] && str[i] != ';' && str[i] != '\n' && str[i] != '}') {
+			std::string single_value;
+			while (str[i] == ' ') {i++;}
+			while (str[i] && str[i] != ';' && str[i] != '\n' && str[i] != '}' && str[i] != ' ') {
+				single_value+= str[i++];
+			}
+			values.push_back(single_value);
+			i++;
+			std::cout << single_value << " ";
+		}
+		if (str[i] && str[i] != '}')
+			i++;
+		std::cout << std::endl;
+		if (values[0] == "listen") {
+			virtual_server.port = atoi(values[1].c_str());
 		}
 	}
+	if (str[i] != '}') {
+		throw std::runtime_error("Ending curly brace missing: '}'");
+	}
+	std::cout << std::endl;
+	return (virtual_server);
 }
 
 Config::Config(const std::string filename)
 {
-	std::cout << "Reading config file: " << filename << std::endl;
+	Virtual_Server_Config virtual_server_obj;
+	std::string file_content = get_file_content(filename);
 
-	std::ifstream file(filename.c_str());
-	std::string line;
-	std::cout << "------------------" << std::endl;
-	while (std::getline(file, line)) {
-		getPort(line);
-		std::cout << GREEN << line << "\n" << RESET;
+	size_t index = file_content.find("server");
+	while (index != std::string::npos) {
+		virtual_server_obj = read_virtual_server_obj(file_content, index + 6);
+		this->virtual_servers.push_back(virtual_server_obj);
+		while (file_content[index] && file_content[index] != '}') {index++;}
+		index = file_content.find("server", index);
 	}
-	std::cout << "------------------" << std::endl;
+}
+
+std::vector<Virtual_Server_Config> Config::get_virtual_servers()
+{
+
+	return (this->virtual_servers);
 }
