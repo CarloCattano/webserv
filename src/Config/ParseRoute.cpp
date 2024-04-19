d1e70ddf69e2fdfa7a73f2a794dfeb30bdc85033
JalbersCode <josephal@hotmail.de>
2024-04-19 13:47:56 +0200

    added parser fastcgi directives

--- src/Config/ParseRoute.cpp
+++ src/Config/ParseRoute.cpp
@@ -31,6 +31,30 @@ void    set_allowed_methods(Route &route, std::vector<std::string> key_with_valu
     }
 }
 
+void parse_location(int size, std::vector<std::string> &key_with_values, Route &route)
+{
+	if (size < 3 || size > 4 || key_with_values[size - 1] != "{")
+		throw std::runtime_error("Bad route format on first line.");
+	if (size == 4)
+		route.matching_style = key_with_values[1];
+	route.location = key_with_values[size - 2];
+}
+
+void parse_redirection(std::vector<std::string> &key_with_values, int size, Route &route)
+{
+	HttpRedirection redirection(key_with_values[size - 1]);
+	if (size == 3) {
+		redirection.code = key_with_values[1];
+	}
+	route.redirections.push_back(redirection);
+}
+
+void parse_param(std::vector<std::string> &key_with_values, Route &route)
+{
+	Fastcgi_Param param(key_with_values[1], key_with_values[2]);
+	route.fastcgi_params.push_back(param);
+}
+
 int	parse_route(Virtual_Server_Config &virtual_server, std::string str, int i) {
 	Route						route;
 	std::vector<std::string> 	key_with_values;
@@ -41,28 +65,24 @@ int	parse_route(Virtual_Server_Config &virtual_server, std::string str, int i) {
 		key_with_values = convert_server_line_2_vector(str, i);
 		size = key_with_values.size();
 
-		if (key_with_values[0] == "location") {
-			if (size < 3 || size > 4 || key_with_values[size - 1] != "{")
-				throw std::runtime_error("Bad route format on first line.");
-			if (size == 4)
-				route.matching_style = key_with_values[1];
-			route.location = key_with_values[size - 2];
-		}
+		if (key_with_values[0] == "location")
+			parse_location(size, key_with_values, route);
 		else if (key_with_values[0] == "root" && size == 2)
 			route.root = key_with_values[1];
-		else if (key_with_values[0] == "return" && size >= 2 && size <= 3) {
-			HttpRedirection redirection(key_with_values[size - 1]);
-			if (size == 3) {
-				redirection.code = key_with_values[1];
-			}
-			route.redirections.push_back(redirection);
-		}
+		else if (key_with_values[0] == "return" && size >= 2 && size <= 3)
+			parse_redirection(key_with_values, size, route);
 		else if (key_with_values[0] == "autoindex" && size == 2 && toLowerCase(key_with_values[1]) == "true")
 			route.autoindex = true;
 		else if (key_with_values[0] == "index" && size >= 2)
 			route.index_files = get_values(key_with_values);
 		else if (key_with_values[0] == "deny" || key_with_values[0] == "allow")
             set_allowed_methods(route, key_with_values);
+		else if (key_with_values[0] == "fastcgi_pass" && size == 2)
+			route.fastcgi_pass = key_with_values[1];
+		else if (key_with_values[0] == "fastcgi_index" && size == 2)
+			route.fastcgi_index = key_with_values[1];
+		else if (key_with_values[0] == "fastcgi_param" && size == 3)
+			parse_param(key_with_values, route);
 		i = iterate_to_next_server_line(str, i);
 	}
 	if (str[i] == '}')
@@ -70,3 +90,5 @@ int	parse_route(Virtual_Server_Config &virtual_server, std::string str, int i) {
 	virtual_server.routes.push_back(route);
 	return (i);
 }
+
+
--- src/Config/ToDo.md
+++ src/Config/ToDo.md
@@ -3,3 +3,41 @@
 ### Allow Post and Get
 Currently implemented a basic version where you can call "deny" and "allow" followed by methods and "all". Methods take precedence over the "all" keyword.
     - Need to find out how far this has to go... Do I need to implement "limit_except"?
+
+### Execute CGI based on certain file extension (for example .php).
+gpt:
+To configure Nginix to run PHP scripts, you would have to use fastcgi_pass directive:
+Example:
+location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass 127.0.0.1:9000;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME /scripts$fastcgi_script_name;
+}
+
+ - Do I need to implement this extension matching? Isnt that?
+
+### 8. Make the route able to accept uploaded files and configure where they should be saved.
+
+For file uploads, you'll have to do some specific configuration and use a server side script to handle the saving of files. Below is an example of the kind of configuration needed:
+Example:
+location /upload {
+    client_max_body_size 10m;
+    fastcgi_pass   localhost:9000;
+    fastcgi_param SCRIPT_FILENAME /scripts$fastcgi_script_name;
+    include        fastcgi_params;
+}
+
+### 9. Your server should work with one CGI (PHP-CGI, Python, etc.)
+gpt
+This might be configuration that is outside of the Nginx configuration file itself. The important part is to set up Nginx to pass the correct information to FastCGI using the fastcgi_param directive.
+For example:
+location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass 127.0.0.1:9000;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    fastcgi_param PATH_INFO $fastcgi_path_info;
+    fastcgi_param PATH_TRANSLATED $document_root$fastcgi_path_info;
+    fastcgi_split_path_info ^(.+\.php)(.*)$;
+}
+This example shows how to pass a .php file to a PHP-CGI handler. The fastcgi_param directives pass additional information to the PHP interpreter.
