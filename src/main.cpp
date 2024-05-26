47f80bd1b8a22c70d78c77b6e9c90639b0fcc9d1
Barra <macmahonbarra+git@gmail.com>
2024-05-26 12:18:36 +0200

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,22 @@
 int main(int argc, char *argv[]) {
 	std::string config_file;
 
-	if (argc == 1) {
-		config_file = "server.conf";
-		std::cout << "Using default configuration file: server.conf" << std::endl;
-	} else if (argc == 2)
-		config_file = argv[1];
-
 	try {
+		if (argc == 1) {
+			config_file = "server.conf";
+			std::cout << "Using default configuration file: server.conf" << std::endl;
+		} else if (argc == 2)
+			config_file = argv[1];
+		else
+			throw std::runtime_error("Usage: ./webserv [config_file]");
+
 		Config config(config_file);
 		ServerCluster cluster(config.get_servers());
 
 		cluster.start();
+
 	} catch (std::exception &e) {
+
 		std::cerr << e.what() << std::endl;
 		return 1;
 	}
